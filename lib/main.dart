b288a17b3ab86bb970260fe7c9c891174f3ebef5
AdrianHernandezIslas <15161315@itoaxaca.edu.mx>
2020-06-01 12:29:41 -0500

    Proyecto de ejemplo terminado

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GET',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
       home: RandomWords(),
     );
   }
 }
+
 //Alamacena los datos en el estado
 class RandomWords extends StatefulWidget {
   @override
@@ -23,35 +28,83 @@ class RandomWords extends StatefulWidget {
   }
 }
 
-class RandomWordsState extends State<RandomWords>{
-
+class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _biggerfont = const TextStyle(fontSize: 18);
+  final _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Lista infinita'
-          )),
-        body:_buildSuggeestions(),
-      );
+      appBar: AppBar(
+        title: Text('Lista infinita'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggeestions(),
+    );
   }
 
-  Widget _buildSuggeestions(){
-    return ListView.builder(
-      itemBuilder: (context, i){
-        if( i >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[i]);
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerfont,
+          ),
+        );
       });
+
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Palabras gurdadas'),
+        ),
+        body: ListView(
+          children: divided,
+        ),
+      );
+    }));
+  }
+
+  Widget _buildSuggeestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          if (i >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          final index = i ~/ 2;
+          return _buildRow(_suggestions[index]);
+        });
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase),
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerfont,
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
-          
 }
